66b0b796e7fed42716740477d163fe28e9d4cfe8
jirayu <jirayu.photisan@gmail.com>
2019-08-04 00:04:01 +0700

    receive sms finish

--- app/src/main/java/com/example/job2/MainActivity.java
+++ app/src/main/java/com/example/job2/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.job2;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.Telephony;
 import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.job.JobInfo;
@@ -9,6 +16,11 @@ import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.util.Log;
 import android.view.View;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.Date;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,10 +30,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(ContextCompat.checkSelfPermission(getBaseContext(), "android.permission-group.SMS") != PackageManager.PERMISSION_GRANTED) {
+            final int REQUEST_CODE_ASK_PERMISSIONS = 123;
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{"android.permission.READ_SMS","android.permission.RECEIVE_SMS","android.permission.READ_SMS"}, REQUEST_CODE_ASK_PERMISSIONS);
+            Log.d(TAG, "Job scheduled");
+        }
+
+
     }
 
     public void scheduleJob(View v) {
-        startService(new Intent(this, TimeService.class));
+        ContentResolver cr = getBaseContext().getContentResolver();
+        Cursor c = cr.query(Telephony.Sms.CONTENT_URI, null, null, null, null);
+        String msgData = "";
+        int totalSMS = 0;
+        if (c != null) {
+            totalSMS = c.getCount();
+            if (c.moveToFirst()) {
+                for (int j = 0; j < totalSMS; j++) {
+                    String smsDate = c.getString(c.getColumnIndexOrThrow(Telephony.Sms.DATE));
+                    String number = c.getString(c.getColumnIndexOrThrow(Telephony.Sms.ADDRESS));
+                    String body = c.getString(c.getColumnIndexOrThrow(Telephony.Sms.BODY));
+                    Date dateFormat= new Date(Long.valueOf(smsDate));
+                    String type;
+                    switch (Integer.parseInt(c.getString(c.getColumnIndexOrThrow(Telephony.Sms.TYPE)))) {
+                        case Telephony.Sms.MESSAGE_TYPE_INBOX:
+                            type = "inbox";
+                            break;
+                        case Telephony.Sms.MESSAGE_TYPE_SENT:
+                            type = "sent";
+                            break;
+                        case Telephony.Sms.MESSAGE_TYPE_OUTBOX:
+                            type = "outbox";
+                            break;
+                        default:
+                            break;
+                    }
+
+
+                    c.moveToNext();
+                }
+            }
+
+            c.close();
+
+        } else {
+            Toast.makeText(this, "No message to show!", Toast.LENGTH_SHORT).show();
+        }
+        Log.d(TAG, msgData);
+
+//        startService(new Intent(this, TimeService.class));
 
 //        ComponentName componentName = new ComponentName(this, ExampleJobService.class);
 //        JobInfo info = new JobInfo.Builder(123, componentName)
